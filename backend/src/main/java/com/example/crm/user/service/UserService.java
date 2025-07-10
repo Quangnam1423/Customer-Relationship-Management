fbb3889538d11d2f05e60c2c9bea8b0ae51000d5
Tong Quang Nam <suongthu2003@gmail.com>
2025-07-10 13:35:47 +0700

    modify UserService

--- backend/src/main/java/com/example/crm/user/service/UserService.java
+++ backend/src/main/java/com/example/crm/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.crm.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.crm.user.model.Role;
 import com.example.crm.user.model.User;
 import com.example.crm.user.repository.UserRepository;
 import com.example.crm.user.dto.UpdateUserRequest;
